1ca72a8db3936a136e966dc17d13d9c1a919f35a
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-30 06:51:26 -0800

    Implemented validation for request body

--- server/src/services/emailService.ts
+++ server/src/services/emailService.ts
@@ -7,3 +7,12 @@ export interface IContactUsForm {
     subject: string
     message: string
 }
+
+export async function validateContactUsForm(reqBody: any): Promise<boolean> {
+    if(!('email' in reqBody) ||
+        !('subject' in reqBody) ||
+        !('message' in reqBody))
+        return false
+    
+    return true
+}
--- server/src/v1/contactus.ts
+++ server/src/v1/contactus.ts
@@ -1,17 +1,21 @@
 import { Request, Response, Router } from 'express'
 
-import { IContactUsForm, sendContactUs } from '../services/emailService'
+import { IContactUsForm, sendContactUs, validateContactUsForm } from '../services/emailService'
 
 const router = Router()
 
 router.post('/', async (req: Request, res: Response) => {
+    if(await validateContactUsForm(req.body) === false){
+        res.status(400).send({message: 'Request was in improper format.'})
+    }
+
     const form = req.body as IContactUsForm
     const success = await sendContactUs(form)
 
     if(success){
         res.send(success)
     } else {
-        res.status(400).send({message: 'Failed to send message.'})
+        res.status(500).send({message: 'Failed to send message.'})
     }
 } )
 
