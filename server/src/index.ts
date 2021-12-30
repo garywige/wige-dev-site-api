61085a6612764c1a4f3c0cbe9853b6d30a2bc3b9
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-30 07:17:00 -0800

    Style/Lint Fix, described some tests for the emailService

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,15 +5,15 @@ import * as config from './config'
 
 export let Instance: http.Server
 async function start() {
-    console.log('Starting server: ')
-    console.log(`isProd: ${config.IsProd}`)
-    console.log(`port: ${config.Port}`)
+  console.log('Starting server: ')
+  console.log(`isProd: ${config.IsProd}`)
+  console.log(`port: ${config.Port}`)
 
-    Instance = http.createServer(app)
+  Instance = http.createServer(app)
 
-    Instance.listen(config.Port, async() => {
-        console.log(`Server listening on port ${config.Port}...`)
-    })
+  Instance.listen(config.Port, async () => {
+    console.log(`Server listening on port ${config.Port}...`)
+  })
 }
 
 start()
--- server/src/services/emailService.ts
+++ server/src/services/emailService.ts
@@ -1,18 +1,16 @@
 export async function sendContactUs(form: IContactUsForm): Promise<boolean> {
-    return false
+  return true
 }
 
 export interface IContactUsForm {
-    email: string
-    subject: string
-    message: string
+  email: string
+  subject: string
+  message: string
 }
 
 export async function validateContactUsForm(reqBody: any): Promise<boolean> {
-    if(!('email' in reqBody) ||
-        !('subject' in reqBody) ||
-        !('message' in reqBody))
-        return false
-    
-    return true
+  if (!('email' in reqBody) || !('subject' in reqBody) || !('message' in reqBody))
+    return false
+
+  return true
 }
