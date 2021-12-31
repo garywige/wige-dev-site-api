e615e07ed445e59db42f624be7bed4f4bcccd0cc
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-31 09:20:53 -0800

    encapsulated emailService code in a class

--- server/src/services/emailService.ts
+++ server/src/services/emailService.ts
@@ -1,15 +1,21 @@
 import { IContactUsForm } from "./interfaces/contact-us-form"
 
-export async function sendContactUs(form: IContactUsForm): Promise<boolean> {
-  console.log(
-    `sendContactUs() called: email address: ${form.email}, subject: ${form.subject}`
-  )
-  return true
+class EmailService{
+  constructor(){}
+
+  async sendContactUs(form: IContactUsForm): Promise<boolean> {
+    console.log(
+      `sendContactUs() called: email address: ${form.email}, subject: ${form.subject}`
+    )
+    return true
+  }
+  
+  async validateContactUsForm(reqBody: any): Promise<boolean> {
+    if (!('email' in reqBody) || !('subject' in reqBody) || !('message' in reqBody))
+      return false
+  
+    return true
+  }
 }
 
-export async function validateContactUsForm(reqBody: any): Promise<boolean> {
-  if (!('email' in reqBody) || !('subject' in reqBody) || !('message' in reqBody))
-    return false
-
-  return true
-}
+export const emailService = new EmailService()
--- server/src/v1/routes/contactus.ts
+++ server/src/v1/routes/contactus.ts
@@ -1,8 +1,7 @@
 import { Request, Response, Router } from 'express'
 
 import {
-  sendContactUs,
-  validateContactUsForm,
+  emailService
 } from '../../services/emailService'
 import { IContactUsForm } from '../../services/interfaces/contact-us-form'
 import { IServerMessage } from '../../services/interfaces/server-message'
@@ -10,13 +9,13 @@ import { IServerMessage } from '../../services/interfaces/server-message'
 const router = Router()
 
 router.post('/', async (req: Request, res: Response) => {
-  if ((await validateContactUsForm(req.body)) === false) {
+  if ((await emailService.validateContactUsForm(req.body)) === false) {
     res.status(400).send({ message: 'Request body is in improper format' } as IServerMessage)
     return
   }
 
   const form = req.body as IContactUsForm
-  const success = await sendContactUs(form)
+  const success = await emailService.sendContactUs(form)
 
   if (success) {
     res.status(200).send(req.body as IContactUsForm)
