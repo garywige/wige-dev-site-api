648d8b6550f1949ca0142159a577a4c73408e9e8
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-31 08:49:33 -0800

    Modified API routing to confirm with designed responses

--- server/src/v1/routes/contactus.ts
+++ server/src/v1/routes/contactus.ts
@@ -1,25 +1,27 @@
 import { Request, Response, Router } from 'express'
 
 import {
-  IContactUsForm,
   sendContactUs,
   validateContactUsForm,
 } from '../../services/emailService'
+import { IContactUsForm } from '../../services/interfaces/contact-us-form'
+import { IServerMessage } from '../../services/interfaces/server-message'
 
 const router = Router()
 
 router.post('/', async (req: Request, res: Response) => {
   if ((await validateContactUsForm(req.body)) === false) {
-    res.status(400).send({ message: 'Request was in improper format.' })
+    res.status(400).send({ message: 'Request body is in improper format' } as IServerMessage)
+    return
   }
 
   const form = req.body as IContactUsForm
   const success = await sendContactUs(form)
 
   if (success) {
-    res.send(success)
+    res.status(200).send(req.body as IContactUsForm)
   } else {
-    res.status(500).send({ message: 'Failed to send message.' })
+    res.status(500).send({ message: 'Message failed to send' } as IServerMessage)
   }
 })
 
