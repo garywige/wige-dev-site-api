aac626ae61b137f72898e5b6b636958695c02883
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-31 08:00:24 -0800

    Copied content from swagger-config file to docs-config

--- server/src/docs-config.ts
+++ server/src/docs-config.ts
@@ -4,7 +4,6 @@ import { Options } from 'swagger-jsdoc'
 const options: Options = {
     swaggerDefinition: {
         openapi: '3.0.1',
-        components: {},
         info: {
             title: 'wige-dev-site-api',
             version: '1.0.0',
@@ -23,7 +22,104 @@ const options: Options = {
                 url: 'https://myprodserver.com',
                 description: 'Production environment'
             }
-        ]
+        ],
+        components: {
+            schemas: {
+                ContactUsForm: {
+                    type: 'object',
+                    properties: {
+                        email: {
+                            type: 'string'
+                        },
+                        subject: {
+                            type: 'string'
+                        },
+                        message: {
+                            type: 'string'
+                        }
+                    }
+                },
+                ServerMessage: {
+                    type: 'object',
+                    properties: {
+                        message: {
+                            type: 'string'
+                        }
+                    }
+                }
+            },
+            responses: {
+                BadFormatError: {
+                    description: 'Request body is improperly formatted',
+                    content: {
+                        'application/json': {
+                            schema: {
+                                $ref: '#/components/schemas/ServerMessage'
+                            }
+                        }
+                    }
+                },
+                InternalError: {
+                    description: 'An internal server error occurred',
+                    content: {
+                        'application/json': {
+                            schema: {
+                                $ref: '#/components/schemas/ServerMessage'
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        paths: {
+            '/v1/contactus': {
+                post: {
+                    description: 'Sends an email to site owner containing the specified content.',
+                    requestBody: {
+                        required: true,
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    $ref: '#/components/schemas/ContactUsForm'
+                                }
+                            }
+                        }
+                    },
+                    responses: {
+                        '200': {
+                            description: 'Success',
+                            content: {
+                                'application/json': {
+                                    schema: {
+                                        $ref: '#/components/schemas/ContactUsForm'
+                                    }
+                                }
+                            }
+                        },
+                        '400': {
+                            description: 'Request body is in improper format',
+                            content: {
+                                'application/json': {
+                                    schema: {
+                                        $ref: '#/components/responses/BadFormatError'
+                                    }
+                                }
+                            }
+                        },
+                        '500': {
+                            description: 'Message failed to send',
+                            content: {
+                                'application/json': {
+                                    schema: {
+                                        $ref: '#/components/responses/InternalError'
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     },
     apis: [
         '**/v1/routes/*.js'
