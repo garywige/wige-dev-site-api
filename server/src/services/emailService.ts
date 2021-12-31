87d7d1778df829a55386af5ba989dd8d7d80de90
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-31 08:34:36 -0800

    created interfaces to conform to spec

--- server/src/services/emailService.ts
+++ server/src/services/emailService.ts
@@ -1,14 +1,10 @@
+import { IContactUsForm } from "./interfaces/contact-us-form"
+
 export async function sendContactUs(form: IContactUsForm): Promise<boolean> {
   console.log(
     `sendContactUs() called: email address: ${form.email}, subject: ${form.subject}`
   )
   return true
-}
-
-export interface IContactUsForm {
-  email: string
-  subject: string
-  message: string
 }
 
 export async function validateContactUsForm(reqBody: any): Promise<boolean> {
